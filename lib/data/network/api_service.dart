b31d734e1c5ff31840310972cd0e0abf1e7d545c
Ebrahim683 <ebrahimrahman20@gmail.com>
2022-12-28 18:13:45 +0600

    api service integration

---
+++ lib/data/network/api_service.dart
@@ -0,0 +1,22 @@
+import 'dart:developer';
+import 'package:dio/dio.dart' as dio;
+
+class ApiService {
+  static String baseUrl = '';
+  static final _dio = dio.Dio();
+
+  static Future<dynamic> getApi(String path, {body}) async {
+    try {
+      final response = await _dio.get(baseUrl + path);
+      if (response.statusCode == 200) {
+        return Future.value(response.data);
+      } else {
+        return Future.error(
+            'api_service: ${response.statusCode}=>${response.statusMessage}');
+      }
+    } catch (e) {
+      log('api_service: ${e.toString()}');
+      return Future.error(e.toString());
+    }
+  }
+}
